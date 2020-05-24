9e9da39ef524db7a55dfb14f2b16e34642098f2b
changjunpyo <changjunpyo@gmail.com>
2020-05-24 12:55:33 +0900

    Dockerfile & Dockercompose

--- server.go
+++ server.go
@@ -2,13 +2,54 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"html/template"
+	"io"
 	"net/http"
+	"crypto/sha256"
+	"encoding/hex"
 )
+
+type TemplateRenderer struct{
+	templates *template.Template
+}
+
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{},c echo.Context) error{
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start("0.0.0.0:1323"))
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+	e.Renderer = renderer
+
+
+	e.GET("/", handleIndex)
+	e.POST("/", handleIndex)
+
+
+	e.GET("/monster/:name", getIdenticon)
+
+	e.Logger.Fatal(e.Start("0.0.0.0:5000"))
+}
+
+func getIdenticon(c echo.Context) error {
+	name := c.Param("name")
+	resp , _ := http.Get(`http://dnmonster:8080/monster/`+name+ `?size=80`)
+	return c.Stream(http.StatusOK, "image/png", resp.Body)
+}
+
+func handleIndex(c echo.Context) error{
+		salt := "UNIQUE"
+		h := sha256.New()
+		saltedName := salt + "Joe malone"
+		if c.FormValue("name") != "" {
+			saltedName = salt+ c.FormValue("name")
+		}
+		h.Write([]byte(saltedName))
+		return c.Render(http.StatusOK, "index.html", map[string]interface{}{
+			"name" : c.FormValue("name"),
+			"name_hash": hex.EncodeToString(h.Sum(nil)),
+		})
 }
